cd0da921ee1c6112aff0a4fde7313af0126132d4
Neal McBurnett <neal@mcburnett.org>
2012-11-15 09:23:49 -0700

    grab just redirect files for each county

--- mine_election.py
+++ mine_election.py
@@ -4,9 +4,13 @@ Parses xml election results file from clarity
 
 TODO:
  parse a collection of clarity downloads
+ look for small vote counts, bad residuals, etc
+ look at diffs between runs
+ print last-updated date and ballot count
 
 """
 
+import urllib
 from collections import Counter
 import lxml.etree as ET
 
@@ -107,22 +111,118 @@ def xpath_unique(parent, path):
         print("Error: path %s found %d times in %s" % (path, nnodes, root))
     return nodes[0]
 
+CO_counties = [
+ 'Adams/43035/',
+ 'Alamosa/43036/',
+ 'Arapahoe/43034/',
+ 'Archuleta/43037/',
+ 'Baca/43038/',
+ 'Bent/43039/',
+ 'Boulder/43040/',
+ 'Broomfield/43041/',
+ 'Chaffee/43042/',
+ 'Cheyenne/43043/',
+ 'Clear_Creek/43044/',
+ 'Conejos/43045/',
+ 'Costilla/43046/',
+ 'Crowley/43047/',
+ 'Custer/43048/',
+ 'Delta/43049/',
+ 'Denver/43050/',
+ 'Dolores/43051/',
+ 'Douglas/43052/',
+ 'Eagle/43053/',
+ 'El_Paso/43055/',
+ 'Elbert/43054/',
+ 'Fremont/43056/',
+ 'Garfield/43057/',
+ 'Gilpin/43058/',
+ 'Grand/43059/',
+ 'Gunnison/43060/',
+ 'Hinsdale/43061/',
+ 'Huerfano/43062/',
+ 'Jackson/43063/',
+ 'Jefferson/43033/',
+ 'Kiowa/43064/',
+ 'Kit_Carson/43065/',
+ 'La_Plata/43067/',
+ 'Lake/43066/',
+ 'Larimer/43068/',
+ 'Las_Animas/43069/',
+ 'Lincoln/43070/',
+ 'Logan/43071/',
+ 'Mesa/43072/',
+ 'Mineral/43073/',
+ 'Moffat/43074/',
+ 'Montezuma/43075/',
+ 'Montrose/43076/',
+ 'Morgan/43077/',
+ 'Otero/43078/',
+ 'Ouray/43079/',
+ 'Park/43080/',
+ 'Phillips/43081/',
+ 'Pitkin/43082/',
+ 'Prowers/43083/',
+ 'Pueblo/43084/',
+ 'Rio_Blanco/43085/',
+ 'Rio_Grande/43086/',
+ 'Routt/43087/',
+ 'Saguache/43088/',
+ 'San_Juan/43089/',
+ 'San_Miguel/43090/',
+ 'Sedgwick/43091/',
+ 'Summit/43092/',
+ 'Teller/43093/',
+ 'Washington/43095/',
+ 'Weld/43096/',
+ 'Yuma/43097/',
+]
+
+def main():
+    if False:
+        for path in CO_counties:
+            stream = urllib.urlopen("http://results.enr.clarityelections.com/CO/" + path)
+            data = stream.read()
+            pathn = path.replace("/", "_")
+            with open(pathn + ".html", "w") as out:
+                out.write(data)
+            print pathn
+
+            # => e.g.  http://results.enr.clarityelections.com/CO/Boulder/43040/110810/en/summary.html
+
+            #print data
+            #import pdb; pdb.set_trace()	# put this where you want to start tracing
+
+    detail_xml = open(detail_xml_name)
+    root = ET.parse(detail_xml).getroot()
+
+    electionName = xpath_unique(root, '//ElectionResult/ElectionName').text
+    timestamp = xpath_unique(root, '//ElectionResult/Timestamp').text
+    ballotsCast = int(xpath_unique(root, '//ElectionResult/ElectionVoterTurnout/@ballotsCast'))
+
+    print "Election Name: %s\nTimestamp: %s\nBallots Cast: %d\n" % (electionName, timestamp, ballotsCast)
+
+    resid = list(residuals(root))
+
+    # avoid some local races
+    top_resid = [r for r in resid if r.residual <= 70.0]
+
+    for r in sorted(top_resid, key=lambda r: r.residual):
+        print r
+
+    contests = {}
+    for r in resid:
+        contests[r.name] = r
+
+    p = contests['PRESIDENT AND VICE PRESIDENT']
+
+    print "Residual %      Votes    Ballots  County name"
+    for c in sorted(p.by_county.values(), key=lambda c: c.residual):
+        print "%10.2f %10d %10d  %s" % (c.residual, c.total, c.ballotsCast, c.name)
+
+print "temporarily init root for interactive use"
 detail_xml = open(detail_xml_name)
 root = ET.parse(detail_xml).getroot()
 
-resid = list(residuals(root))
-
-# avoid some local races
-top_resid = [r for r in resid if r.residual <= 70.0]
-
-for r in sorted(top_resid, key=lambda r: r.residual):
-    print r
-
-contests = {}
-for r in resid:
-    contests[r.name] = r
-
-p = contests['PRESIDENT AND VICE PRESIDENT']
-print "Residual %      Votes    Ballots  County name"
-for c in sorted(p.by_county.values(), key=lambda c: c.residual):
-    print "%10.2f %10d %10d  %s" % (c.residual, c.total, c.ballotsCast, c.name)
+if __name__ == "__main__":
+    main()
